08ec728574120fa930dbde8a1388faa0ae447103
Anastasia Chernyaeva <moiri@mail.ru>
2022-01-27 08:05:21 +0300

    changes

--- Chernyaeva_Anastasia_dz_1/task_1_1.py
+++ Chernyaeva_Anastasia_dz_1/task_1_1.py
@@ -1,24 +1,4 @@
-def convert_time(duration: int) -> str:
-    if duration < 60:
-        return (f'{duration} сек')
-    else:
-        min = duration // 60
-        sec = duration % 60
-        if min < 60:
-            return (f'{min} мин {sec} сек')
-        else:
-            hour = min // 60
-            min = min % 60
-            if hour < 24:
-                return (f'{hour} час {min} мин {sec} сек')
-            else:
-                day = hour // 24
-                hour = hour % 24
-                return (f'{day} дн {hour} час {min} мин {sec} сек')
-
-duration = 400153
-result = convert_time(duration)
-print(result) #test1
+joke_list = ['лес сегодня мягкий', 'огонь сегодня зеленый', 'дом позавчера зеленый', 'автомобиль сегодня яркий', 'город ночью мягкий', 'дом сегодня веселый', 'огонь сегодня зеленый', 'автомобиль вчера зеленый', 'лес сегодня мягкий', 'город ночью яркий']
 
 
 
